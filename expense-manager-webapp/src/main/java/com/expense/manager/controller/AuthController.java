5219aca65d8985870ce90819b303b2c4362dc4c2
Deshmukh <2238186@cognizant.com>
2024-07-01 02:20:03 +0530

    Feature - Register form validations

--- expense-manager-webapp/src/main/java/com/expense/manager/controller/AuthController.java
+++ expense-manager-webapp/src/main/java/com/expense/manager/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.expense.manager.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.expense.manager.dto.UserDTO;
 import com.expense.manager.service.UserService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -18,7 +20,7 @@ public class AuthController {
 	private final UserService userService;
 
 	@GetMapping({"/","/login"})
-	public String showLoginPage() {
+	public String showLoginPage(){
 		return "login";
 	}
 	
@@ -29,8 +31,10 @@ public class AuthController {
 	}
 	
 	@PostMapping("/register")
-	public String register(@ModelAttribute("user") UserDTO userDTO, Model model) {
-		System.out.println("printing the user details :"+userDTO);
+	public String register(@Valid @ModelAttribute("user") UserDTO userDTO,  BindingResult result,Model model) {
+		if(result.hasErrors()) {
+			return "register";
+		}
 		userService.save(userDTO);
 		model.addAttribute("successMessage", true);
 		return "login"; //return view name instead of redirect. If we redirect we won't have access to successMessage
--- expense-manager-webapp/src/main/java/com/expense/manager/dto/UserDTO.java
+++ expense-manager-webapp/src/main/java/com/expense/manager/dto/UserDTO.java
@@ -1,5 +1,8 @@
 package com.expense.manager.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +12,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserDTO {
 	
+	@NotBlank(message="User name should not be null")
 	private String name;
+	
+	@Email(message="Invalid email address")
 	private String email;
+	
+	@NotBlank(message="Password should not be null")
+	@Size(min=5,message = "Password should be minimum {min} characters")
 	private String password;
+	
 	private String confirmpassword;
 
 }
